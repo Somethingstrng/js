43d0357419c33e12ed39e81cd64892603ed23dd3
Somethingstrng <elli.born1@gmail.com>
2021-09-14 21:34:40 +0300

    lc

--- js/index.js
+++ js/index.js
@@ -91,3 +91,117 @@ let helloFunc=function(){
 }
 let resHelloFunc=helloFunc;
 console.log(resHelloFunc);
+//func declaration
+function myAdd1(x1,x2){
+    return x1+x2;
+}
+//func expression
+let myAdd2=function(x1,x2){
+    return x1+x2;
+}
+console.log(myAdd1(2,2));
+console.log(myAdd2(2,2));
+
+//array function
+let myAdd3=(x1,x2)=>x1+x2;
+//or
+let myAdd4=(x1,x2)=>{
+    let result;
+    result=x1+x2;
+    return result;//important
+}
+console.log(myAdd3(2,2));
+console.log(myAdd4(2,2));
+
+let sayHelloWithUser=(userName)=>{
+    let greeting='Hello,'+userName;
+    console.log(greeting);
+}
+
+//вывести числа от введенного до 0
+let myFunc1=userValue=>{
+    let i;
+    for(i=userValue; i>=0; i--){
+        console.log(i);
+    }
+}
+myFunc1(10);
+//or
+let myFunc1=userValue=>{
+    for(userValue; userValue>=0; userValue--){
+        console.log(userValue);
+    }
+}
+myFunc1(10);
+//or
+let myFunc1=userValue=>{
+    while(userValue>=0){
+        console.log(userValue);
+        userValue--;
+    }
+}
+myFunc1(10);
+
+//Определите в какую четверть часа попадает ввыденное количество минут пользователем (например, 40 минут - 3-я четверть часа)
+let userValue=(minutes)=>{
+    let part=Math.ceil(minutes/15);
+    return part;
+}
+
+let userValue=(minutes)=>{
+    let part=Math.ceil(minutes/15);
+    switch(part){
+        case 1:{
+            console.log('1 part');
+            break;
+        }
+        case 2:{
+            console.log('2 part');
+            break;
+        }
+        case 3:{
+            console.log('3 part');
+            break;
+        }
+        case 4:{
+            console.log('4 part');
+            break;
+        }
+        default: {
+            console.log('error');
+            break;
+        }
+    }
+}
+
+let userValue=prompt('Enter minutes');
+console.log('Is' definePartOfHour(userValue));
+
+//Найти произведение чисел в пределах от limit1 до limit2
+
+let limit=(limit1,limit2)=>{
+    if(limit1>limit2){
+        let buf=limit1;
+        limit1=limit2;
+        limit2=buf;
+    }
+    let i,result=1;
+    for(i=limit1;i<=limit2;i++){
+        result*=i;
+    }
+    return result;
+}
+//or
+let limit=(limit1,limit2)=>{
+    if(limit1>limit2){
+        let buf=limit1;
+        limit1=limit2;
+        limit2=buf;
+    }
+    let result=1;
+    for(;limit1<=limit2;limit1++){
+        result*=limit1;
+    }
+    return result;
+}
+console.log(limit(1,8));
